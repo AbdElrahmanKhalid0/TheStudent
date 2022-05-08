579c5398942918ce6b84a8ab50c82d3f946e4ac4
Abdelrahman Khalid <abdelrahman.elzohry2014@gmail.com>
2022-05-08 12:37:50 +0200

    made the welcome View Scrollable 🖱️

--- components/WelcomePage/WelcomePage.js
+++ components/WelcomePage/WelcomePage.js
@@ -1,115 +1,117 @@
-import { View, Image, StyleSheet, Text, Dimensions, TouchableOpacity, Animated } from 'react-native'
+import { View, Image, StyleSheet, Text, Dimensions, TouchableOpacity, Animated, ScrollView } from 'react-native'
 import React, {useRef, useState} from 'react'
 import MarterialIcons from 'react-native-vector-icons/MaterialIcons';
-
-const DATA = [
-    {
-    title:'Study in One Place',
-    brief:'In here you can study all your subjects with your friends and from your favourable lecturers.',
-    img:'learn'
-    },
-    {
-    title:'Share',
-    brief:'You can share your thoughts with everyone you want in The community.',
-    img:'share'
-    },
-    {
-    title:'Study With Friends',
-    brief:'You can choose your study partner/s in order to study together and encourage each other',
-    img:'study_together'
-    },
-    {
-    title:'Graduate With the highest scores',
-    brief:'After studying, solving large amounts of questions, and stirring your curisity you (inshallah) will score the highest scores',
-    img:'graduate'
-    },
-]
 
 const MARGIN = Dimensions.get('screen').width / 2
 const WelcomePage = () => {
 
     const containerAnim = useRef(new Animated.Value(MARGIN*3)).current;
-    const [currentViewMargin, setCurrentViewMargin] = useState(MARGIN*3);
+    const [currentViewMargin, setCurrentViewMargin] = useState(0);
+
+    const scrollView = useRef();
 
     const nextView = () => {
-        if(currentViewMargin > -MARGIN*3) {
-            Animated.timing(containerAnim, {
-                toValue:currentViewMargin-2*MARGIN,
-                duration: 1000,
-                useNativeDriver: true,
-            }).start()
-            setCurrentViewMargin(currentViewMargin-2*MARGIN)
-        }
-    }
-    const previousView = () => {
-        if(currentViewMargin < MARGIN*3) {
-            Animated.timing(containerAnim, {
-                toValue:currentViewMargin+2*MARGIN,
-                duration: 1000,
-                useNativeDriver: true,
-            }).start()
+        if(currentViewMargin < MARGIN*6) {
             setCurrentViewMargin(currentViewMargin+2*MARGIN)
         }
+        scrollView.current.scrollTo({x: currentViewMargin+2*MARGIN});
+    }
+    const previousView = () => {
+        if(currentViewMargin > 0) {
+            setCurrentViewMargin(currentViewMargin-2*MARGIN)
+        }
+        scrollView.current.scrollTo({x: currentViewMargin-2*MARGIN});
     }
 
   return (
     <View style={{flex:1,backgroundColor:'white'}}>
-        <Animated.View style={[styles.container, {transform:[{translateX:containerAnim}]}]}>
+        <ScrollView 
+            style={{flex:1}}
+            horizontal={true}
+            ref={scrollView}
+            showsHorizontalScrollIndicator={false}
+            onScrollEndDrag={({nativeEvent}) => {
+                const factor = ((nativeEvent.contentOffset.x-currentViewMargin)/(2*MARGIN)).toString();
+                const scrollToRatio = parseFloat("0"+factor.slice(factor.indexOf(".")));
 
-            <View style={styles.page}>
-            <Image 
-                source={require('../../assets/WelcomePage/learn.jpg')}
-                style={styles.img}
-            />
-            <Text style={[styles.title,{color: '#796fe2'}]}>
-                Learn
-            </Text>
-            <Text style={[styles.brief, {color:'#3d3c6f'}]}>
-                In here you can study all your subjects with your friends and from your favourable lecturers.
-            </Text>
+                if (factor > 0) {
+                    // scroll right
+                    if (scrollToRatio > .3 && currentViewMargin < MARGIN*6) {
+                        scrollView.current.scrollTo({x:currentViewMargin+2*MARGIN});
+                        setCurrentViewMargin(currentViewMargin+2*MARGIN);
+                    } else {
+                        scrollView.current.scrollTo({x:currentViewMargin});
+                    }
+                } else if (factor < 0) {
+                    // scroll left
+                    if (scrollToRatio > .3 && currentViewMargin > 0) {
+                        scrollView.current.scrollTo({x:currentViewMargin-2*MARGIN});
+                        setCurrentViewMargin(currentViewMargin-2*MARGIN);
+                    } else {
+                        scrollView.current.scrollTo({x:currentViewMargin});
+                    }
+                    
+                }
+            }}
+        >
+
+            <View style={styles.container}>
+
+                <View style={styles.page}>
+                    <Image 
+                        source={require('../../assets/WelcomePage/learn.jpg')}
+                        style={styles.img}
+                    />
+                    <Text style={[styles.title,{color: '#796fe2'}]}>
+                        Learn
+                    </Text>
+                    <Text style={[styles.brief, {color:'#3d3c6f'}]}>
+                        In here you can study all your subjects with your friends and from your favourable lecturers.
+                    </Text>
+                </View>
+
+                <View style={styles.page}>
+                    <Image 
+                        source={require('../../assets/WelcomePage/share.jpg')}
+                        style={[styles.img,{marginRight:MARGIN/8}]}
+                    />
+                    <Text style={[styles.title,{color: '#b75741'}]}>
+                        Share
+                    </Text>
+                    <Text style={[styles.brief, {color:'#4a231b'}]}>
+                        You can share your thoughts with everyone you want in The community.
+                    </Text>
+                </View>
+
+
+                <View style={styles.page}>
+                    <Image 
+                        source={require('../../assets/WelcomePage/study_together.jpg')}
+                        style={[styles.img,{height:300}]}
+                    />
+                    <Text style={[styles.title,{color: '#82b379'}]}>
+                        Study With Friends
+                    </Text>
+                    <Text style={[styles.brief, {color:'#2c3d29'}]}>
+                        You can choose your study partner/s in order to study together and encourage each other.
+                    </Text>
+                </View>
+
+                <View style={styles.page}>
+                    <Image 
+                        source={require('../../assets/WelcomePage/graduate.jpg')}
+                        style={[styles.img,{height:300}]}
+                    />
+                    <Text style={[styles.title,{color: '#1e458a'}]}>
+                        Graduate With the highest scores
+                    </Text>
+                    <Text style={[styles.brief, {color:'#142443'}]}>
+                        After studying, solving large amounts of questions, and stirring your curisity you (inshallah) will score the highest scores
+                    </Text>
+                </View>
+
             </View>
-
-            <View style={styles.page}>
-            <Image 
-                source={require('../../assets/WelcomePage/share.jpg')}
-                style={[styles.img,{marginRight:MARGIN/8}]}
-            />
-            <Text style={[styles.title,{color: '#b75741'}]}>
-                Share
-            </Text>
-            <Text style={[styles.brief, {color:'#4a231b'}]}>
-                You can share your thoughts with everyone you want in The community.
-            </Text>
-            </View>
-
-
-            <View style={styles.page}>
-            <Image 
-                source={require('../../assets/WelcomePage/study_together.jpg')}
-                style={[styles.img,{height:300}]}
-            />
-            <Text style={[styles.title,{color: '#82b379'}]}>
-                Study With Friends
-            </Text>
-            <Text style={[styles.brief, {color:'#2c3d29'}]}>
-                You can choose your study partner/s in order to study together and encourage each other.
-            </Text>
-            </View>
-
-            <View style={styles.page}>
-            <Image 
-                source={require('../../assets/WelcomePage/graduate.jpg')}
-                style={[styles.img,{height:300}]}
-            />
-            <Text style={[styles.title,{color: '#1e458a'}]}>
-                Graduate With the highest scores
-            </Text>
-            <Text style={[styles.brief, {color:'#142443'}]}>
-                After studying, solving large amounts of questions, and stirring your curisity you (inshallah) will score the highest scores
-            </Text>
-            </View>
-
-        </Animated.View>
+        </ScrollView>
 
         <TouchableOpacity style={styles.previousBtn} activeOpacity={.7} onPress={previousView}>
             <MarterialIcons name='arrow-back-ios' color='white' size={30} style={{marginLeft:7}}/>
@@ -125,7 +127,8 @@ export default WelcomePage;
 
 const styles = StyleSheet.create({
     container: {
-        flex:1,
+        height:"100%",
+        width:"100%",
         backgroundColor:'white',
         justifyContent:'center',
         alignItems:'center',
@@ -133,13 +136,13 @@ const styles = StyleSheet.create({
     },
     page: {
         height:'100%',
-        width:'100%',
+        width:Dimensions.get('screen').width,
         justifyContent:'center',
         alignItems:'center'
     },
     img:{
         height:400,
-        width:'100%'
+        width:Dimensions.get('screen').width
     },
     title: {
         fontSize: 40,
